ff462287b1710c5bf6cef4f95581bc41de3e149e
AstralTurtle <jamesacs329@gmail.com>
2023-11-29 13:55:56 +0000

    added projects.js

---
+++ projects.js
@@ -0,0 +1,10 @@
+let projects = [];
+let projectDropdown = document.getElementById("project-dropdown");
+// projects by id
+// SFoRS = 0
+projects.push(document.querySelector("#sfors"));
+
+projectDropdown.addEventListener("onchange", () => {
+    alert("hello!")
+})
+
