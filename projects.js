5659ccabf1896de77d7b2488fa7a2660a2d4b109
AstralTurtle <jamesacs329@gmail.com>
2023-11-30 16:29:39 -0500

    11/30 vscode

--- projects.js
+++ projects.js
@@ -1,10 +1,28 @@
 let projects = [];
 let projectDropdown = document.getElementById("project-dropdown");
-// projects by id
-// SFoRS = 0
-projects.push(document.querySelector("#sfors"));
+// games by id
+// SFoRS = 1
+// PsuedoSneak = 2
+// Drill Deeper = 3
+// Slime Slayer = 4
+
+gameFrame = document.getElementById("gameFrame");
+
+
+const projectArr =[
+    "",
+    "https://itch.io/embed-upload/8740303?color=000000",
+    "https://itch.io/embed-upload/9012549?color=333333",
+    "https://itch.io/embed-upload/8578275?color=111111",
+    "https://itch.io/embed-upload/7358114?color=132E32",
+];
+
 
 projectDropdown.addEventListener("onchange", () => {
-    alert("hello!")
+    gameFrame.src = projectArr[projectDropdown.value];
+    gameFrame.contentWindow.location.reload();
+    console.log(projectDropdown.value);
 })
+// make a button
+
 
